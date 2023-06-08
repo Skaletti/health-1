717dc1e17edda646dd9e661e51252886447b41be
Skaletti <gordey.korolev.2016@mail.ru>
2023-06-08 19:44:54 +0700

    feature update

--- scripts/modules/index.js
+++ scripts/modules/index.js
@@ -2,8 +2,24 @@ import './core/swiper-script.min.js'
 
 new Swiper('.swiper', {
     loop: true,
+    slidesPerView: 2.5,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        480: {
+            slidesPerView: 2.1,
+        },
+        768: {
+            slidesPerView: 2.3,
+        },
+        1000: {
+            slidesPerView: 2.4,
+        }
+    }
 });
